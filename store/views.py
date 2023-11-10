1c2ce85d3b781acf94530c951a66574a4a893848
darko <ki14.skakun@gmail.com>
2023-11-10 18:45:58 +0200

    user login

--- store/views.py
+++ store/views.py
@@ -26,3 +26,27 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'store/registration.html', context)
+
+
+def user_login(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except Exception as exc:
+            messages.error(request, 'User does not exists.')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Wrong credentials.')
+    return render(request, 'store/login.html')
