4a044df0996d76f9c293274025f3210df0dcf629
darko <ki14.skakun@gmail.com>
2023-11-12 11:00:35 +0200

    added products related db models

--- store/models.py
+++ store/models.py
@@ -3,9 +3,35 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
     username = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ProductInventory(models.Model):
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.SET_NULL, null=True, related_name='products')
+    inventory = models.OneToOneField(
+        ProductInventory, on_delete=models.CASCADE)
