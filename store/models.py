04d0f88383d5a8d4ba5fe72716b42ebe1fcc04a9
darko <ki14.skakun@gmail.com>
2023-11-15 15:12:54 +0200

    added db models for shopping process

--- store/models.py
+++ store/models.py
@@ -46,3 +46,32 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ShoppingSession(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    session = models.ForeignKey(ShoppingSession, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OderDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OderItem(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    order_detail = models.ForeignKey(OderDetails, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
